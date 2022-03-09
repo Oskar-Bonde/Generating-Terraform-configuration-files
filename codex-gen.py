c69993ca78d5fc083e405e6049d739aa14049d47
Oskar Bonde <oskar.bonde@gmail.com>
2022-03-09 22:31:44 +0100

    generated 1 sample for each

--- codex-gen.py
+++ codex-gen.py
@@ -1,20 +1,61 @@
 import os
 import openai
+import time
 
-openai.api_key = "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"
+# sample is a dict with index key. It contains a list of touples with promt and solution
 
-def get_context():
-    return str(input())
-
+class data_class:
+    def __init__(self):
+        self.path = "data\evaluation"
+        self.files = {}
+        self.raw_file = {}
+        self.read_files()
+        
+    def read_files(self):
+        file_num = -1
+        for filename in sorted(os.listdir(self.path)):
+            with open(os.path.join(self.path, filename), 'r') as f:
+                file_num += 1
+                prompts = []
+                solutions = []
+                #self.raw_file[file_num] = f.read()
+                for line in f.readlines()[1:]:
+                    if line[0] == "#":
+                        prompts.append(line)
+                        solutions.append("")
+                    else: 
+                        solutions[-1] = solutions[-1] + line
+                self.files[file_num] = (prompts, solutions)
+        self.num_files = file_num
+        
+    def generate_tf(self, index):
+        context_file = open("data/context.txt", "r")
+        context = context_file.read()
+        output = ""
+        for prompt in self.files[index][0]:
+            output = output + prompt
+            generated = openai.Completion.create(
+                engine="code-davinci-001", #  code-cushman-001
+                prompt= context+output,
+                max_tokens = 256,
+                top_p = 0.05,
+                #temperature=0.1,
+                n = 1,
+                stop = "\n}\n",
+                echo = False
+            )
+            output = output + generated["choices"][0]["text"] +"\n}\n\n"
+        generated_file = open("data/result/generated_"+str(index)+".txt", "w")
+        generated_file.write(output)
+        
+    def evaluate_all(self):
+        for i in range(29, self.num_files):
+            print(i)
+            self.generate_tf(i)
+            time.sleep(40)
+        
+        
 if __name__ == "__main__":
-    generated = openai.Completion.create(
-        engine="code-cushman-001", #code-davinci-001
-        prompt= get_context(),
-        max_tokens = 256,
-        top_p = 0.05,
-        #temperature=0.1,
-        n = 1,
-        stop = "}\n\n"
-    )
-    text = generated["choices"][0]["text"]
-    print(text)
+    openai.api_key = "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"#os.getenv("KEY")
+    data = data_class()
+    data.evaluate_all()
