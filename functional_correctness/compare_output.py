5cd60b13b25b3631d0b7e35df9e290402ae48bd8
Oskar Bonde <oskar.bonde@gmail.com>
2022-03-16 08:40:52 +0100

    work on fc

--- functional_correctness/compare_output.py
+++ functional_correctness/compare_output.py
@@ -3,13 +3,12 @@ import os
 import re
 
 def read_json(file_path):
-    os.system("terraform -chdir="+str(file_path)+" show -json binary > "+str(file_path)+"/plan.json")
+    #os.system("terraform -chdir="+str(file_path)+" init")
+    #os.system("terraform -chdir="+str(file_path)+" plan -out=binary")
+    #os.system("terraform -chdir="+str(file_path)+" show -json binary > "+str(file_path)+"/plan.json")
     with open(str(file_path)+"/plan.json", "r", encoding='utf-8', errors='ignore') as file:
-        #print(file.read())
         stringPlan = file.read()
-        stringPlan = re.sub('"address":.+?,','',stringPlan)
-        stringPlan = re.sub('"description":.+?,','',stringPlan)
-        stringPlan = re.sub('"name":.+?,','',stringPlan)
+        stringPlan = re.sub('(,?)"(description|address|name)":".+?"(,?)','',stringPlan)
         stringPlan = re.sub('"variables":.+?}},','',stringPlan)
         #plan = json.loads(stringPlan)
         plan = stringPlan
@@ -27,14 +26,15 @@ def re_test():
     str1 = re.sub('"variables":.+?}},','',str1)
     print(str1)
     str2 = '"resources":[{"address":"aws_instance.ec2demo_alt","mode":"managed"'
-    str2 = re.sub('"address":.+?,','',str2)
 
     str3 = '"default":"ami-0915bcb5fa77e4892","description":"AMI ID"},"region"'
     str4 = '"values":{"description":"Dev VPC SSH","egress"'
-    str3 = re.sub('"description":.+?,|}$','',str3)
-    str4 = re.sub('"description":.+?,|}$','',str4)
+    str3 = re.sub('(,?)"(description|address|name)":".+?"(,?)','',str3)
+    str2 = re.sub('(,?)"(description|address|name)":".+?"(,?)','',str2)
+    str4 = re.sub('(,?)"(description|address|name)":".+?"(,?)','',str4)
     print(str3)
     print(str4)
+    print(str2)
 
 if __name__ == "__main__":
     #re_test()
@@ -42,7 +42,8 @@ if __name__ == "__main__":
     plan2 = read_json("file_2")
     plan3 = read_json("file_3")
     plan4 = read_json("file_4")
+    print("file 3", plan3)
     assert plan1 == plan2
     assert plan1 == plan3
-
+    
 
